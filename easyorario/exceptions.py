813c42074531f461a0820d2297e54e36204d4ad2
Giorgio Vilardo <giorgio.vilardo@nearform.com>
2026-02-17 11:53:44 +0100

    add auth exceptions and Italian i18n error messages

--- easyorario/exceptions.py
+++ easyorario/exceptions.py
@@ -3,3 +3,29 @@
 
 class EasyorarioError(Exception):
     """Base exception for all Easyorario domain errors."""
+
+    error_key: str = ""
+
+
+class EmailAlreadyTakenError(EasyorarioError):
+    """Raised when registration email is already in use."""
+
+    error_key = "email_taken"
+
+
+class PasswordTooShortError(EasyorarioError):
+    """Raised when password doesn't meet minimum length."""
+
+    error_key = "password_too_short"
+
+
+class InvalidEmailError(EasyorarioError):
+    """Raised when email format is invalid."""
+
+    error_key = "invalid_email"
+
+
+class PasswordMismatchError(EasyorarioError):
+    """Raised when password and confirmation don't match."""
+
+    error_key = "password_mismatch"
---
+++ easyorario/i18n/errors.py
@@ -0,0 +1,9 @@
+"""Italian error and success message mappings."""
+
+ERRORS: dict[str, str] = {
+    "email_taken": "Questo indirizzo email è già registrato",
+    "password_too_short": "La password deve contenere almeno 8 caratteri",
+    "password_mismatch": "Le password non corrispondono",
+    "invalid_email": "Indirizzo email non valido",
+    "registration_success": "Registrazione completata. Effettua l'accesso.",
+}
